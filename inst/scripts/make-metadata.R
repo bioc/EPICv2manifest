977aef78222f480e58a05557268579f07bad17f8
timpet <t.peters@garvan.org.au>
2024-02-10 16:59:19 +1100

    EPICv2manifest_0.99.1

--- inst/scripts/make-metadata.R
+++ inst/scripts/make-metadata.R
@@ -6,10 +6,10 @@
 meta <- data.frame(
   Title = "EPICv2manifest",
   Description = "A data.frame containing an extended probe manifest for the Illumina Infinium Methylation v2.0 Kit. Contains the complete manifest from the Illumina-provided EPIC-8v2-0_EA.csv, plus additional probewise information described in Peters et al. (2024).",
-  BiocVersion = "3.18",
+  BiocVersion = "3.19",
   Genome = "hg38",
   SourceType = "CSV", 
-  SourceUrl = "https://bioconductor.org/packages/release/data/annotation/html/EPICv2manifest.html", #Placeholder until publication
+  SourceUrl = "https://bioconductor.org/packages/EPICv2manifest", #Placeholder until publication
   SourceVersion = "Feb 03 2024",
   Species = "Homo sapiens",
   TaxonomyId = 9606,
@@ -18,6 +18,6 @@ meta <- data.frame(
   Maintainer = "Tim Peters <t.peters@garvan.org.au>",
   RDataClass = "data.frame",
   DispatchClass = "Rda",
-  RDataPath = "EPICv2manifest.Rda")
+  RDataPath = "EPICv2manifest/EPICv2manifest.Rda")
 
 write.csv(meta, file="inst/extdata/metadata.csv", row.names=FALSE)
